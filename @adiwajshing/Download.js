51039efbb1fafda7a78e030437bfb38e62119d10
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-10-10 11:55:15 +0700

    Create Download.js

---
+++ @adiwajshing/Download.js
@@ -0,0 +1,26 @@
+import { writeFile } from 'fs/promises'
+import { downloadMediaMessage } from '@adiwajshing/baileys'
+
+sock.ev.on('messages.upsert', async ({ messages }) => {
+    const m = messages[0]
+
+    if (!m.message) return // if there is no text or media message
+    const messageType = Object.keys (m.message)[0]// get what type of message it is -- text, image, video
+    // if the message is an image
+    if (messageType === 'imageMessage') {
+        // download the message
+        const buffer = await downloadMediaMessage(
+            m,
+            'buffer',
+            { },
+            { 
+                logger,
+                // pass this so that baileys can request a reupload of media
+                // that has been deleted
+                reuploadRequest: sock.updateMediaMessage
+            }
+        )
+        // save to file
+        await writeFile('./my-download.jpeg', buffer)
+    }
+}
