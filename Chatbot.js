23b09e82e59cf75d976dde0072657d9363778711
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-10-10 11:20:52 +0700

    Create Chatbot.js

---
+++ Chatbot.js
@@ -0,0 +1,26 @@
+WAConnection
+WAWeb
+WABot
+{
+  Msg.Reply}=${command}menu
+  Msg.bot}=${mb.js}
+}
+{
+  Msg.Reply}=${command}start
+  Msg.bot}=Hello ${name} mau main anonymous chat? Yuk langsung tekan ajaa tombol dibawah/n/n/n/${footer=Chatbot Autoresponder}/n/n/n/${button1=Main}/n/n/n/${button2=stop}
+}
+{
+  Msg.Reply}=${send}>{${command}start
+  Msg.bot1}=Sedang mencari partner..../n/n/n/${footer=Chatbot Autorespond}
+  Msg.bot2}=Partner ketemu!/n/n/n${footer=.send untuk mengirimkan nomer}
+}
+{
+  fake.replyfile}=whatsapp.fake/reply/~self.command
+  fake.replyfile}=whatsapp.fake/reply/~public.command
+  fake.replyname}=matchbox/n/whatsapp.fake/~reply
+
+//jangan ubah apapun juga
+//author by matchbox
+//bisa dijual kalau izin owner
+
+//© WhatsApp Footer Again Connection
