8f2add045ba200fc02f57a128f4e1316c53d9e59
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-10-10 11:37:09 +0700

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,12 @@
+{
+  "name_owner:Jason"
+  "name_owner2:facebook"
+  "name_owner3:zeeone"
+  "name_bot:MATCHBOX-MD"
+  "nomer_owner:6285868055463, 6285936528795, 6281392189144"
+  "nomer_ownerw:6285868055463@whatsapp.net","6285936528795@whatsapp.net","6281392189144@whatsapp.net"
+  "apikey:nasigoreng551"
+  "apizeks:alphabotmd"
+  "apisecurity:mb761haiytqg"
+  "kodeprem:PREMBANH"
+}
